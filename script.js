cdfa6a357f106793e741a78411d9a34fe444025b
FosterChad95 <fosterchad95@gmail.com>
2021-04-23 16:03:22 -0400

    Added Additional Functionality

--- script.js
+++ script.js
@@ -2,37 +2,49 @@ const list = document.querySelector(".todo-list");
 const todoInput = document.querySelector(".todo-input");
 const checkboxParent = document.querySelector(".todo");
 const clearCompleted = document.querySelector(".clear");
+const numberItems = document.querySelector(".number");
 let counter = 0;
 
 function addTodo(event) {
   const html = `
-  <li class="todo-item">
-  <input type="checkbox" class="todo-checkbox" />
-  <span class="text">${event.target.value}</span>
-  </li>`;
+    <li class="todo-item">
+    <input type="checkbox" class="todo-checkbox" />
+    <span class="text">${event.target.value}</span>
+    </li>`;
   list.insertAdjacentHTML("afterbegin", html);
+  event.target.value = "";
   localStorage.setItem("list", list.innerHTML);
   counter++;
+  itemsLeft();
 }
 
 function removeTodo(element) {
   if (!element) return;
-  element.classList.toggle("checked");
   const todoText = document.querySelector(".text");
-  todoText.classList.toggle("striked");
+  element.classList.toggle("checked");
+  element.nextElementSibling.classList.toggle("striked");
   localStorage.setItem("list", list.innerHTML);
-  counter--;
+
+  element.classList.contains("checked") && counter >= 0 ? counter-- : counter++;
+
+  itemsLeft();
 }
 
-function clearTodo() {}
+function clearTodo() {
+  list.innerHTML = "";
+  localStorage.setItem("list", list.innerHTML);
+}
 
-function itemsLeft() {}
+function itemsLeft() {
+  numberItems.textContent = counter + " ";
+}
 
 function init() {
   //Loading Local Storage on Load
   list.innerHTML = localStorage.getItem("list");
   localStorage.clear();
 
+  itemsLeft();
   //EVENT LISTENERS
 
   clearCompleted.addEventListener("click", clearTodo);
